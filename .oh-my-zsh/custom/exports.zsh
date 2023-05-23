d331ed685ba57b6e54e63d3434697543f780d5dc
Benjamin Rush <bnjmnrsh@gmail.com>
2023-05-23 16:45:39 +0100

    Move Volta path exports

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -11,3 +11,8 @@ export NODE_PATH=`which node`
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Volta - JavaScript tool manager
+# https://volta.sh/
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
